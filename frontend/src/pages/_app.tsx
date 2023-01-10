f7ee30004661626a946483e57d3a7ea600a56771
Douglas Araujo <dhoouglas@hotmail.com>
2023-01-10 12:59:33 -0300

    Component Header and style

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,10 +2,12 @@ import '../ui/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material';
 import tema from '../ui/themes/tema';
+import Header from '../ui/components/Header/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
     <ThemeProvider theme={tema}>
+      <Header />
       <Component {...pageProps} />
     </ThemeProvider>
   );   
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <div>Testando</div>
+    <div></div>
   );
 }
