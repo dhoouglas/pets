cb1d5f51862fdc5d0ebad15b43488dd910a5e1de
Douglas Araujo <dhoouglas@hotmail.com>
2023-01-10 12:35:52 -0300

    Created Thema

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../ui/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../ui/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material';
+import tema from '../ui/themes/tema';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <ThemeProvider theme={tema}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );   
 }
