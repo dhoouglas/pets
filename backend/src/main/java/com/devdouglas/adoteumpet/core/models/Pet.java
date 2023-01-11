829706180bc4bf7808d36339600eda34e2698127
Douglas Araujo <dhoouglas@hotmail.com>
2023-01-11 14:39:26 -0300

    Created entitie Pet

---
+++ backend/src/main/java/com/devdouglas/adoteumpet/core/models/Pet.java
@@ -0,0 +1,40 @@
+package com.devdouglas.adoteumpet.core.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity()
+public class Pet implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private String history;
+	
+	@Column(nullable = false)
+	private String photo;
+	
+	
+	
+}
