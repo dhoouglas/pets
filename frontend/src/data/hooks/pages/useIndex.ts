c73178647b381f46e304bf2a43e38e87989d4503
Douglas Araujo <dhoouglas@hotmail.com>
2023-01-13 14:34:27 -0300

    Config method Post, and clear form

--- frontend/src/data/hooks/pages/useIndex.ts
+++ frontend/src/data/hooks/pages/useIndex.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Pet } from "../../@types/Pet";
 import { ApiService } from "../../services/ApiService";
+import { AxiosError } from "axios";
 
 export function useIndex() {
     const [listPets, setListPets] = useState<Pet[]>([]),
@@ -14,10 +15,43 @@ export function useIndex() {
             .then((response) => {
                 setListPets(response.data)
             })
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        if(petSelected === null) {
+            clearForm();
+        }
+    }, [petSelected]);
 
     function adotar() {
+        if(petSelected !== null) {
+            if(validateDataAdocao()) {
+                ApiService.post('/adocoes', {
+                    pet_id: petSelected.id,
+                    email,
+                    valor: value
+                })
+                    .then(() => {
+                        setPetSelected(null);
+                        setMessage('Parabéns Pet adotado com sucesso!');
+                        clearForm();
+                    })
+                    .catch((error: AxiosError) => {
+                        setMessage(error.request?.data.message);
+                    })
+            } else {
+                setMessage('Preencha todos os campos corretamente! ')
+            }
+        }
+    }
 
+    function validateDataAdocao() {
+        return email.length > 0 && value.length > 0;
+    }
+
+    function clearForm() {
+        setEmail('');
+        setValue('');
     }
           
     return {
